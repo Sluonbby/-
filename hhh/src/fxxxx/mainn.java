b742bf9e605117e9305731d5e1984464f46cbc1d
b0tch33 <664414314@qq.com>
2019-10-13 16:11:56 +0800

    哦哦哦哦哦
    噢噢噢噢

---
+++ hhh/src/fxxxx/mainn.java
@@ -0,0 +1,16 @@
+package fxxxx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainn extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

701a0670937304efa15aa11b38b4674e44b8c1e2
b0tch33 <664414314@qq.com>
2019-10-19 15:53:48 +0800

    javafx_builder_text

--- javafx_bulider_text/src/application/Main.java
+++ javafx_bulider_text/src/application/Main.java
@@ -1,19 +1,18 @@
 package application;
-
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("aaaaaaaaaaaaaaaaaaa.fxml"));//修改了
+            Parent root = FXMLLoader.load(getClass().getResource("Piano_demo.fxml"));//修改了
             //BorderPane root = new BorderPane();
             //设置Scene的大小(SceneBuilder中点击AnchorPane右边Layout中会显示大小,不一样的可以)
-            Scene scene = new Scene(root,500,522);//修改了
+            Scene scene = new Scene(root);//修改了
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);//设置不能窗口改变大小
@@ -27,5 +26,4 @@ public class Main extends Application {
         launch(args);
     }
 }
-
 
--- javafx_bulider_text/src/application/Piano_demoController.java
+++ javafx_bulider_text/src/application/Piano_demoController.java
@@ -6,15 +6,15 @@ import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
 
-public class MyController {
+public class Piano_demoController {
 	@FXML
 	private Button bt1;
 	@FXML
-	private TextField text1;
-
+	private TextField tf1;
+	
 	public void eventButton(){
-        String text = text1.getText();//获取文本框输入的内容
+        String text = tf1.getText();//获取文本框输入的内容
         System.out.println(text);
     }
-	
+
 }

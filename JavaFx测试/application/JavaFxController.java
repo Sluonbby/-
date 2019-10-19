d87e9dbbb65c5211eca48d98685c4b192aad725a
Sluonbby <56494862+Sluonbby@users.noreply.github.com>
2019-10-19 16:07:21 +0800

    爽的

---
+++ JavaFx测试/application/JavaFxController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class JavaFxController {
+	@FXML
+	private Button Button1;
+	public void eventButton() {
+		System.out.print("청唐묾끓뎨앎청唐劤櫓벌\n가가가가가가가가가가가가가\n가가가가가가가가가가가가가\n가가가가가가가가가가가가가\n");
+	}
+
+}

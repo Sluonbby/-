cdc37de0ada687d6f22702eb9bd60bd9cf83166e
b0tch33 <664414314@qq.com>
2019-10-25 23:43:30 +0800

    钢琴能出声了！！！！！！！！！！！！！！！

--- javafx_bulider_text/src/application/Piano_demoController.java
+++ javafx_bulider_text/src/application/Piano_demoController.java
@@ -6,7 +6,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class Piano_demoController {
 	@FXML
@@ -17,12 +19,21 @@ public class Piano_demoController {
 	private ImageView white2;
 	@FXML
 	private Button bt1;
+	@FXML
+	private Media media;
+	@FXML
+	private MediaPlayer mplayer;
+	
 
 	public void press_Q_Clicked(KeyEvent event) {
 		if(event.getCode() == KeyCode.Q){
 			tf1.setText("Click Q");
+			File file = new java.io.File("a48.mp3");
+			media = new Media(file.toURI().toString());
+			mplayer = new MediaPlayer(media);
 			white1.setVisible(false);
 			white2.setVisible(true);
+			mplayer.play();
 			String text = tf1.getText();
 			System.out.println(text);
 		}
